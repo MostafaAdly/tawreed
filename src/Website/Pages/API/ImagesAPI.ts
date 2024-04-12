457776a7ff23ecaa68bdcec67a41bd109be55e8c
MostafaAdly <68769237+MostafaAdly@users.noreply.github.com>
2024-04-12 22:24:42 +0200

    fixed images api

--- src/Website/Pages/API/ImagesAPI.ts
+++ src/Website/Pages/API/ImagesAPI.ts
@@ -7,13 +7,13 @@ export default class ImagesAPI extends Page {
     private data: any;
     public static base_url: string = "/images";
     constructor(data: any, base_url?: string) {
-        super(base_url || ImagesAPI.base_url);
+        super(base_url + ImagesAPI.base_url);
         this.data = data;
         this.run();
     }
 
     private run() {
-        this.router.get("/images/:id", (req: any, res: any) => {
+        this.router.get("/:id", (req: any, res: any) => {
             const id = req.params.id;
             const ps = new stream.PassThrough();
             console.log("Reviewing image", path.join(process.cwd(), `/LocalDatabase/images/${id}`))
