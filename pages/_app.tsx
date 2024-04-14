2ae2701f500ffd1e86baeaef75dd5afe1b100272
MostafaAdly <68769237+MostafaAdly@users.noreply.github.com>
2024-04-15 00:55:54 +0200

    Changed website's description for a better one.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,10 +16,14 @@ import '../public/Assets/FontAwesome/css/all.css'
 import '../public/Global/css/style.css'
 import '../styles/globals.css'
 import { getImage } from '../public/Assets/Helpers'
-// -------------- 
+// --------------
 
 // ================================================================= [ Main Application ]
 export default function MyApp({ Component, pageProps }: AppProps) {
+
+  const description = `تُفتح لك بوابة المستقبل في عملية الشراء! انضم إلينا لتتمتع بتوطين عمليتك بواسطة منصتنا المُشغَّلة بالذكاء الاصطناعي. قم بتبسيط سير العمل، وتعزيز علاقاتك مع المورّدين، وتأمين عمليات الشراء في أقل من ساعتين. استمتع بتوفير التكاليف على الشحن والموظفين، بالإضافة إلى الوصول إلى عروض أفضل. خذ السيطرة، وقم باتخاذ قرارات أكثر ذكاءً بسرعة، واستمتع بتجربة مستقبلية في عمليات الشراء من خلال تجربة مجانية!`;
+
+
   return (
     <div>
       <Head>
@@ -29,10 +33,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <title>Tawreed | توريد</title>
         <link rel="icon" type="image/x-icon" href={getImage("grey-logo.png")}></link>
         <meta name="keywords" content="Tawreed, توريد, مشتريات, مبيعات, مصر" />
-        <meta name="description" content="الموقع الأول في مصر لربط المشتريات والمبيعات" />
+        <meta name="description" content={description} />
 
         <meta property="og:title" content="Tawreed | توريد" />
-        <meta property="og:description" content="الموقع الأول في مصر لربط المشتريات والمبيعات" />
+        <meta property="og:description" content={description} />
         <meta property="og:image" content="http://adlyy.me:3000/api/v1/images/grey-logo.png" />
         <meta property="og:url" content="https://tec-tawreed.com/" />
         <meta property="og:site_name" content="T.E.C Tawreed" />
@@ -41,7 +45,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Tawreed | توريد" />
-        <meta name="twitter:description" content="الموقع الأول في مصر لربط المشتريات والمبيعات" />
+        <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content="http://adlyy.me:3000/api/v1/images/grey-logo.png" />
 
         {/* <Script src='https://kit.fontawesome.com/e6c13277a1.js' crossOrigin='anonymous' />  */}
