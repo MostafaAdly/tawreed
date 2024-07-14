b3771226d8c7a0c3a71e0140f7dc55c26358bdea
MostafaAdly <MostafaAdly.official@gmail.com>
2024-07-14 22:48:22 +0300

    edited pages/admin/users/edit.tsx

--- pages/admin/users/edit.tsx
+++ pages/admin/users/edit.tsx
@@ -32,17 +32,70 @@ const EditUser = () => {
                         <div className="grid gap-6 mb-6 md:grid-cols-2 py-3">
                             <div className="flex flex-col gap-6 mb-2">
                                 <label htmlFor="email" className="text-sm font-medium text-gray-900 dark:text-gray-300">البريد الإلكتروني</label>
-                                <input ref={emailRef} type="email" id="email" name="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                <input placeholder="email@example.com" ref={emailRef} type="email" id="email" name="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                             </div>
                             <div className="flex flex-col gap-6 mb-2">
                                 <label htmlFor="phone" className="text-sm font-medium text-gray-900 dark:text-gray-300">الهاتف</label>
-                                <input ref={phoneRef} type="text" id="phone" name="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                <input placeholder="01xxxxxx341" ref={phoneRef} type="text" id="phone" name="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                             </div>
                         </div>
                         <button onClick={onSearch} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             بحث
                         </button>
                     </div>
+                )
+            }
+            {
+                users.length > 1 && (
+                    <div className="block w-full p-6 mb-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"></h5>
+                        {/* SEARCH RESULTS */}
+
+
+                        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3">
+
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Color
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Category
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Price
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Action
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Apple MacBook Pro 17"
+                                        </th>
+                                        <td className="px-6 py-4">
+                                            Silver
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            Laptop
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            $2999
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div >
                 )
             }
             {
@@ -61,6 +114,12 @@ const EditUser = () => {
         </AdminLayout >
     )
 };
+
+const SearchResult = ({ user }) => {
+    return (
+        
+    )
+}
 
 export default EditUser;
 
