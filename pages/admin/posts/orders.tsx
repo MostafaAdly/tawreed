0fc24a251009a9a0d8a4005c21cd4c1341dc1436
MostafaAdly <MostafaAdly.official@gmail.com>
2024-07-24 15:32:37 +0300

    fixed some bugs

--- pages/admin/posts/orders.tsx
+++ pages/admin/posts/orders.tsx
@@ -124,6 +124,7 @@ const Index = ({ user }) => {
 
 
 const TableRow = ({ index, order, performAction }) => {
+    console.log(order);
     return (
         <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-slate-100 even:dark:bg-gray-800 border-b dark:border-gray-700">
             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -139,7 +140,7 @@ const TableRow = ({ index, order, performAction }) => {
                 {order.quantity}
             </td>
             <td className="px-6 py-4">
-                {order.offerResponse?.totalPrice}
+                {order?.offerResponse?.totalPrice}
             </td>
             <td className="px-6 py-4">
                 {order.client.username}
